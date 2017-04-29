eb26ab02272e26ba57ae7b8cefabc6cb3d8775f2
kolyan <vkont4@gmail.com>
2017-04-29 12:51:23 +0300

    added mybatis config

--- sn-common/sn-util/src/main/java/ua/social/network/rule/JsonContentLoader.java
+++ sn-common/sn-util/src/main/java/ua/social/network/rule/JsonContentLoader.java
@@ -1,6 +1,5 @@
 package ua.social.network.rule;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
@@ -9,7 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
 
 /**
  * @author Mykola Yashchenko
@@ -18,7 +16,6 @@ public class JsonContentLoader extends TestWatcher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonContentLoader.class);
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final String DEFAULT_FILE_NAME = "request";
     private static final String PATH = "json/%s/%s/%s.json";
 
@@ -26,27 +23,23 @@ public class JsonContentLoader extends TestWatcher {
 
     private Class<?> testClass;
 
-    public JsonContentLoader(Class<?> testClass) {
-        Objects.requireNonNull(testClass);
-        this.testClass = testClass;
-    }
-
     @Override
     protected void starting(Description description) {
+        this.testClass = description.getTestClass();
         this.methodName = description.getMethodName();
     }
 
-    public <T> T prepare(Class<T> requestClass) throws IOException {
-        return prepare(requestClass, DEFAULT_FILE_NAME);
+    public String load() throws IOException {
+        return load(DEFAULT_FILE_NAME);
     }
 
-    public <T> T prepare(Class<T> requestClass, String fileName) throws IOException {
+    public String load(String fileName) throws IOException {
         String path = String.format(PATH, testClass.getSimpleName(), methodName, fileName);
         LOGGER.info("Reading file: {}", path);
         try (InputStream resourceAsStream = testClass.getClassLoader().getResourceAsStream(path)) {
             String json = IOUtils.toString(resourceAsStream);
             LOGGER.info("Loaded JSON: \n{}", json);
-            return MAPPER.readValue(json, requestClass);
+            return json;
         }
     }
 }

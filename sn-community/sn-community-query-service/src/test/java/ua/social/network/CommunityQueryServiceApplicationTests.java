ee359fccf76a3ce90680f66a9f0ee809dc35ac05
kolyan <vkont4@gmail.com>
2017-05-16 22:56:16 +0300

    updated commons io version added test to verify context loading

--- sn-community/sn-community-query-service/src/test/java/ua/social/network/CommunityQueryServiceApplicationTests.java
+++ sn-community/sn-community-query-service/src/test/java/ua/social/network/CommunityQueryServiceApplicationTests.java
@@ -1,7 +1,19 @@
 package ua.social.network;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @author Mykola Yashchenko
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CommunityQueryServiceApplication.class)
 public class CommunityQueryServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
 }
--- sn-community/sn-community-service/src/test/java/ua/social/network/CommunityServiceApplicationTests.java
+++ sn-community/sn-community-service/src/test/java/ua/social/network/CommunityServiceApplicationTests.java
@@ -3,12 +3,12 @@ package ua.social.network;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Mykola Yashchenko
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = CommunityServiceApplication.class)
 public class CommunityServiceApplicationTests {
 

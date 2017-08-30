d18f432eb2e7a92a7295b0efc8407a5d6101fe87
kolyan <vkont4@gmail.com>
2017-08-30 22:29:08 +0300

    Fixed SQL for community tests

--- sn-community/sn-community-query-service/src/test/resources/community/communities.sql
+++ sn-community/sn-community-query-service/src/test/resources/community/communities.sql
@@ -1,5 +1,5 @@
 INSERT INTO community (id, name, create_date, description, user_id) VALUES ('1', 'COMMUNITY-1', TIMESTAMP '2017-04-02 13:30:14', 'COMMUNITY-1-DESC', 'USER-1-ID');
 
-INSERT INTO post (id, text, create_date) VALUES ('1', 'TEST1', TIMESTAMP '2017-04-02 13:30:14');
+INSERT INTO post (id, text, create_date, user_id) VALUES ('1', 'TEST1', TIMESTAMP '2017-04-02 13:30:14', 'USER-2-ID');
 
 INSERT INTO post_community_assoc (community_id, post_id) VALUES ('1', '1');

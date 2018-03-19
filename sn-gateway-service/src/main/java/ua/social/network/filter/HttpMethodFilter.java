4cdfa9a1e7d3acf6bedd2ec1037e1879c4d9c719
kolyan <vkont4@gmail.com>
2018-03-19 08:58:12 +0200

    Added HTTP method filter to route requests between query/command service

---
+++ sn-gateway-service/src/main/java/ua/social/network/filter/HttpMethodFilter.java
@@ -0,0 +1,76 @@
+package ua.social.network.filter;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+/**
+ * @author Mykola Yashchenko
+ */
+public class HttpMethodFilter extends ZuulFilter {
+
+    private static final List<String> QUERY_METHODS = List.of("GET");
+    private static final List<String> COMMAND_METHODS = List.of("POST", "PUT", "DELETE");
+
+    private final DiscoveryClient discoveryClient;
+    private final String commandService;
+    private final String queryService;
+    private final String requestPath;
+
+    public HttpMethodFilter(DiscoveryClient discoveryClient, String commandService,
+                            String queryService, String requestPath) {
+        this.discoveryClient = discoveryClient;
+        this.commandService = commandService;
+        this.queryService = queryService;
+        this.requestPath = requestPath;
+    }
+
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String requestURI = request.getRequestURI();
+        return requestURI.contains(requestPath);
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        String httpMethod = context.getRequest().getMethod();
+
+        List<ServiceInstance> instances;
+        if (QUERY_METHODS.contains(httpMethod)) {
+            instances = discoveryClient.getInstances(queryService);
+        } else {
+            instances = discoveryClient.getInstances(commandService);
+        }
+
+        try {
+            if (CollectionUtils.isNotEmpty(instances)) {
+                context.setRouteHost(instances.get(0).getUri().toURL());
+            } else {
+                throw new IllegalStateException("Target service instance not found!");
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Couldn't get service URL!", e);
+        }
+
+        return null;
+    }
+}

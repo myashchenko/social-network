e446b76ad19891372c8e5bb1053cb178b79554cf
kolyan <vkont4@gmail.com>
2018-03-19 08:43:30 +0200

    Fixed unit tests in sn-user-service

--- sn-user/sn-user-service/src/test/resources/user/users.sql
+++ sn-user/sn-user-service/src/test/resources/user/users.sql
@@ -3,8 +3,8 @@ INSERT INTO users (id, name, created, email, password, role) VALUES ('2', 'USER-
 INSERT INTO users (id, name, created, email, password, role) VALUES ('3', 'USER-3', TIMESTAMP '2017-04-02 13:30:14', 'USER-3@EMAIL.COM', '11111111', 'USER');
 INSERT INTO users (id, name, created, email, password, role) VALUES ('4', 'USER-4', TIMESTAMP '2017-04-02 13:30:14', 'USER-4@EMAIL.COM', '11111111', 'USER');
 
-INSERT INTO friend_request (id, from_id, to_id, created) VALUES ('1', '3', '4', TIMESTAMP '2017-04-02 13:30:14');
-INSERT INTO friend_request (id, from_id, to_id, created) VALUES ('2', '1', '4', TIMESTAMP '2017-04-02 13:30:14');
+INSERT INTO friend_requests (id, from_id, to_id, created) VALUES ('1', '3', '4', TIMESTAMP '2017-04-02 13:30:14');
+INSERT INTO friend_requests (id, from_id, to_id, created) VALUES ('2', '1', '4', TIMESTAMP '2017-04-02 13:30:14');
 
 INSERT INTO user_friends (user_id, friend_id) VALUES ('2', '4');
 INSERT INTO user_friends (user_id, friend_id) VALUES ('4', '2');

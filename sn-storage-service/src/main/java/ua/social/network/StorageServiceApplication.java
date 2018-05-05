9d70fb4bdc6eb290c275f50880048a46d271a595
kolyan <vkont4@gmail.com>
2018-05-05 15:42:09 +0300

    Added storage service bootstrap

---
+++ sn-storage-service/src/main/java/ua/social/network/StorageServiceApplication.java
@@ -0,0 +1,15 @@
+package ua.social.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mykola Yashchenko
+ */
+@SpringBootApplication
+public class StorageServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(StorageServiceApplication.class, args);
+    }
+}

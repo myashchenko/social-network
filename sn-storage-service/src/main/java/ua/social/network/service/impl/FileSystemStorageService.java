dcb2f2f6bdefca27e78b22054df9d393af0956ea
kolyan <vkont4@gmail.com>
2018-05-07 14:14:17 +0300

    Removed unused import statement

--- sn-storage-service/src/main/java/ua/social/network/service/impl/FileSystemStorageService.java
+++ sn-storage-service/src/main/java/ua/social/network/service/impl/FileSystemStorageService.java
@@ -1,7 +1,6 @@
 package ua.social.network.service.impl;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
